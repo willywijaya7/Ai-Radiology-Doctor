973b4ff59f8e79aeb6e1f9392805ce8c886e0cd3
willywijaya7 <68785043+willywijaya7@users.noreply.github.com>
2025-09-03 20:35:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -155,4 +155,5 @@ def cleanup_expired_sessions():
             del analysis_sessions[token]
 
 if __name__ == '__main__':
-    app.run(debug= False, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
